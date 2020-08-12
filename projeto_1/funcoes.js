70c653c84017fc6e20d51c58dfcc5f91028d83c5
Willian Oliveira <williangd@yahoo.com.br>
2020-08-12 00:48:56 -0300

    Aula 56

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 function lerDiretorio(caminho) {
   return new Promise((resolve, reject) => {
-    const arquivos = fs.readdirSync(caminho).map((arquivo) => path.join(caminho, arquivo));
+    const arquivos = fs.readdirSync(caminho).map(arquivo => path.join(caminho, arquivo));
     resolve(arquivos);
   });
 }
 
 function elementosTerminadosCom(padrao) {
   return function (array) {
-    return array.filter((item) => item.endsWith(padrao));
+    return array.filter(item => item.endsWith(padrao));
   };
 }
 
@@ -26,24 +26,46 @@ function lerArquivo(caminho) {
 }
 
 function lerArquivos(caminhos) {
-  return Promise.all(caminhos.map((caminho) => lerArquivo(caminho)));
+  return Promise.all(caminhos.map(caminho => lerArquivo(caminho)));
 }
 
 function removerElementosSeVazio(array) {
-  return array.filter((el) => el.trim());
+  return array.filter(el => el.trim());
 }
 
 function removerElementosSeIncluir(padrao) {
   return function (array) {
-    return array.filter((el) => !el.includes(padrao));
+    return array.filter(el => !el.includes(padrao));
   };
 }
 
 function removerElementosSeApenasNumero(array) {
-  return array.filter((el) => {
-    const num = parseInt(el.trim());
+  return array.filter(el => {
+    const num = parseInt(el.trim(), 10);
     return num !== num;
   });
+}
+
+function removerSimbolos(simbolos) {
+  return function (array) {
+    return array.map(el => {
+      let novoTexto = el;
+      simbolos.forEach(simbolo => {
+        novoTexto = novoTexto.split(simbolo).join('');
+      });
+      return novoTexto;
+    });
+  };
+}
+
+function mesclarElementos(conteudos) {
+  return conteudos.join(' ');
+}
+
+function separarPor(simbolo) {
+  return function (texto) {
+    return texto.split(simbolo);
+  };
 }
 
 module.exports = {
@@ -53,4 +75,7 @@ module.exports = {
   removerElementosSeVazio,
   removerElementosSeIncluir,
   removerElementosSeApenasNumero,
+  removerSimbolos,
+  mesclarElementos,
+  separarPor,
 };
--- projeto_1/index.js
+++ projeto_1/index.js
@@ -1,14 +1,22 @@
-const fn = require('./funcoes');
 const path = require('path');
+const fn = require('./funcoes');
 
 const dir = path.join(__dirname, '..', 'dados', 'legendas');
+
+const simbolos = [
+  '.', '?', '-', '"', ',', '♪', '_', '<i>', '</i>', '\r', '[', ']', '(', ')',
+];
 
 fn.lerDiretorio(dir)
   .then(fn.elementosTerminadosCom('.srt'))
   .then(fn.lerArquivos)
-  .then((conteudos) => conteudos.join('\n'))
-  .then((todoConteudo) => todoConteudo.split('\n'))
+  .then(fn.mesclarElementos)
+  .then(fn.separarPor('\n'))
   .then(fn.removerElementosSeVazio)
   .then(fn.removerElementosSeIncluir('-->'))
   .then(fn.removerElementosSeApenasNumero)
+  .then(fn.removerSimbolos(simbolos))
+  .then(fn.mesclarElementos)
+  .then(fn.separarPor(' '))
+  .then(fn.removerElementosSeVazio)
   .then(console.log);
