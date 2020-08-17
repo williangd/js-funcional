be91808659acf05fe18e7b33ad685c9b6380579b
Willian Oliveira <williangd@yahoo.com.br>
2020-08-16 23:52:13 -0300

    Alterada a função ordernarPorAtributoNumerico

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -80,7 +80,7 @@ function ordernarPorAtributoNumerico(attr, ordem = 'asc') {
   return function (array) {
     const asc = (o1, o2) => o1[attr] - o2[attr];
     const desc = (o1, o2) => o2[attr] - o1[attr];
-    return array.sort(ordem === 'asc' ? asc : desc)
+    return [...array].sort(ordem === 'asc' ? asc : desc)
   }
 }
 
